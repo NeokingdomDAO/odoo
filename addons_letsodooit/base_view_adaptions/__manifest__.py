cadd50c6728910753c4c6f0d9800ed153685f74d
Peter Schwarz <pe.schwarz@hotmail.de>
2023-01-13 11:40:20 +0100

    Remove sidebar for users by default

---
+++ addons_letsodooit/base_view_adaptions/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Base Views',
+    'version' : '16.0.1.0.0',
+    'author': 'Peter Schwarz (info@peter-schwarz.it)',
+    'sequence': 1,
+    'description': """This module adapts the base modules' views accordingly.""",
+    'license': 'OPL-1',
+    'depends': ['base', 'muk_web_theme'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ addons_letsodooit/base_view_adaptions/models/res_users.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    sidebar_type = fields.Selection(default='invisible')
