174d2d5594e7d1599bdeb01b6c4f192882d961b8
Peter Schwarz <pe.schwarz@hotmail.de>
2023-01-13 17:23:00 +0100

    Add necessary access rights for ethereum address field

--- addons_letsodooit/base_user_ethereum_address/models/res_users.py
+++ addons_letsodooit/base_user_ethereum_address/models/res_users.py
@@ -3,4 +3,16 @@ from odoo import fields, models
 class User(models.Model):
     _inherit = 'res.users'
 
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + [
+            'ethereum_address',
+        ]
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + [
+            'ethereum_address',
+        ]
+
     ethereum_address = fields.Char('Ethereum Address')
