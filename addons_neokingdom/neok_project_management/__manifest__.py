d636a3c8108fdc20bff6cdbb7e7021616558bf40
benhrisdev <benhrisdev@gmail.com>
2024-03-19 09:31:21 +0100

    PM tagging - both neok. and proj. specific tagging

--- addons_neokingdom/neok_project_management/__manifest__.py
+++ addons_neokingdom/neok_project_management/__manifest__.py
@@ -2,14 +2,15 @@
 {
     'name': 'NEOKingdom Project Management',
     'version': '16.0.1.0.0',
-    'sequence': 31,
+    'sequence': 101,
     'category': 'NEOKingdom',
     'author': 'NEOKingdom Odoo Team (https://www.neokingdom.org)',
     'description': "Adding new tagging, filtering and other ways to track, configure project management to fit NEOKingdom's needs.",
-    'depends': ['base', 'project'],
+    'depends': ['base', 'project', 'project_task_type_attribution', 'project_view_adaptions'],
     'data': [
         'data/tags.xml',
         'views/project_project.xml',
+        'views/project_task.xml',
     ],
     'installable': True,
     'auto_install': False,
