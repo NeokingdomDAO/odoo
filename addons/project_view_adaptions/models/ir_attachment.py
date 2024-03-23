c6ac4fa72efa6f9f3b6e4b14500743ca62a2a524
benhrisdev <benhrisdev@gmail.com>
2024-03-23 22:25:31 +0100

    multi record create bug fix on Default task cover image from messages

--- addons/project_view_adaptions/models/ir_attachment.py
+++ addons/project_view_adaptions/models/ir_attachment.py
@@ -8,14 +8,18 @@ class IrAttachment(models.Model):
     def create(self, values):
         res = super().create(values)
 
-        # if it's a task attachment, and it's an image add it as cover image
-        if values.get('res_model') == 'project.task':
-            _res_id = values.get('res_id', False)
-
-            if _res_id:
-                _task = self.env['project.task'].browse(_res_id)
-
-                if _task.exists() and not _task.displayed_image_id.exists() and res.mimetype.startswith('image'):
-                    _task.displayed_image_id = res.id
+        if isinstance(values, list):
+            if any((att_val.get('res_model') == 'project.task') for att_val in values):
+                res.add_img_attchment_as_task_cover_img()
+        elif values.get('res_model') == 'project.task':
+            res.add_img_attchment_as_task_cover_img()
 
         return res
+
+    def add_img_attchment_as_task_cover_img(self):
+        # if it's a task attachment, and it's an image add it as cover image
+        for _att in self:
+            _task = self.env['project.task'].browse(_att.res_id)
+
+            if _task.exists() and not _task.displayed_image_id.exists() and _att.mimetype.startswith('image'):
+                _task.displayed_image_id = _att.id
